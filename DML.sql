1b0c51a17d6cc5effb618b9fd8d4a3a23db74289
labsfiap <labsfiap@PA304MICRO14.fiap.com.br>
2025-02-10 22:39:17 -0300

    todo o dml, toma thiaguinho

---
+++ DML.sql
@@ -0,0 +1,30 @@
+INSERT INTO pais(nome_pais) VALUES ('Brasil');
+INSERT INTO pais(nome_pais) VALUES ('Argentina');
+INSERT INTO pais(nome_pais) VALUES ('Chile');
+INSERT INTO pais(nome_pais) VALUES ('Uruguai');
+INSERT INTO pais(nome_pais) VALUES ('Venezuela');
+
+INSERT INTO estado(nome_estado) VALUES ('São Paulo');
+INSERT INTO estado(nome_estado) VALUES ('Rio de Janeiro');
+INSERT INTO estado(nome_estado) VALUES ('Minas Gerais');
+INSERT INTO estado(nome_estado) VALUES ('Espirito Santo');
+INSERT INTO estado(nome_estado) VALUES ('Goias');
+
+INSERT INTO cidade(nome_cidade) VALUES ('São Paulo');
+INSERT INTO cidade(nome_cidade) VALUES ('Niteroi');
+INSERT INTO cidade(nome_cidade) VALUES ('Belo Horizonte');
+INSERT INTO cidade(nome_cidade) VALUES ('Indaiatuba');
+INSERT INTO cidade(nome_cidade) VALUES ('Piracicaba');
+
+INSERT INTO bairro(nome_bairro) VALUES ('Planalto Paulista');
+INSERT INTO bairro(nome_bairro) VALUES ('Rocinha');
+INSERT INTO bairro(nome_bairro) VALUES ('Ibirité');
+INSERT INTO bairro(nome_bairro) VALUES ('Jabaquara');
+INSERT INTO bairro(nome_bairro) VALUES ('Piracicaba');
+
+INSERT INTO endereco_cliente(cep,logradouro,numero,referencia) VALUES ('123','Rua Tal','12','perto do fulano');
+INSERT INTO endereco_cliente(cep,logradouro,numero,referencia) VALUES ('234','Rua Blau','23','perto do ciclano');
+INSERT INTO endereco_cliente(cep,logradouro,numero,referencia) VALUES ('345','Rua Nseioq','34','perto do beltrano');
+INSERT INTO endereco_cliente(cep,logradouro,numero,referencia) VALUES ('456','Rua Rua','45','perto do thigas');
+INSERT INTO endereco_cliente(cep,logradouro,numero,referencia) VALUES ('567','Rua Minha','56','perto do donny');
+
